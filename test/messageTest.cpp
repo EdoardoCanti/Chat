778846d00cca110a9aad7dac4f9fab43347d9738
Edoardo Canti <edoardo.canti.mail@gmail.com>
2024-01-31 11:42:49 +0100

    Added tests for Message c'tor, getters and  setters.

--- test/messageTest.cpp
+++ test/messageTest.cpp
@@ -1,3 +1,20 @@
 //
 // Created by Edoardo Canti on 30/01/24.
 //
+
+#include "gtest/gtest.h"
+#include "../Message.h"
+
+TEST(Message, contentTest){
+    string testString = "Hello World";
+    Message* msg = new Message(testString);
+    auto content = msg->getContent();
+    ASSERT_EQ(testString, content);
+}
+
+TEST(Message, setContent){
+    string stringToSet = "A placeholder testing content.";
+    Message* msg = new Message("HelloWorld!");
+    msg->setContent(stringToSet);
+    ASSERT_EQ(stringToSet, msg->getContent());
+}
