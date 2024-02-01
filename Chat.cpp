1d614f09a12e7900ecd09deb554dcb9b6458f614
Edoardo Canti <edoardo.canti.mail@gmail.com>
2024-02-01 10:36:18 +0100

    Implemented seen messages and unseen messages counter. Implementation changes.

--- Chat.cpp
+++ Chat.cpp
@@ -3,17 +3,24 @@
 //
 
 #include "Chat.h"
+#include <iostream>
 
 int Chat::global_id = 0; // static int set to 0
 
 Chat::Chat() {
     global_id++;
     this->id = global_id;
+    this->messages_counter = 0;
+} // every new Chat has an incremented identifier
 
-} // every new Chat has an incremented identfier
 
 void Chat::addMessage(Message *msg) {
     messages.push_back(msg);
+    messages_counter += 1;
+}
+
+list<Message*> Chat::getMessages(){
+    return messages;
 }
 
 string Chat::showMessages() {
@@ -24,6 +31,33 @@ string Chat::showMessages() {
     return conversation;
 }
 
+int Chat::getMessagesCounter() {
+    return messages_counter;
+}
+
 int Chat::getId(){
     return id;
 }
+
+void Chat::addMember(User* usr){
+    members.push_back(usr);
+}
+
+list<User*> Chat::getMembers(){
+    return members;
+}
+
+int Chat::getSeenCounter() {
+    int seen_counter = 0;
+    for (auto it: messages) {
+        if (it->isSeen().first && it->isSeen().second.size() == getMembers().size())
+            seen_counter += 1;
+    }
+    return seen_counter;
+}
+
+int Chat::getUnseenCounter(){
+    int seen_counter = getSeenCounter();
+    return getMessagesCounter() - seen_counter;
+}
+
--- Chat.h
+++ Chat.h
@@ -7,23 +7,35 @@
 
 #include <list>
 #include "Message.h"
+//include "User.h"
 /*
  * Chat will be composed by a static int identifier
  * and a list of messages
  * check User.h for implementation details.
  */
+
+class User;
+
 class Chat {
 
 private:
     static int global_id;
     int id;
     list<Message*> messages;
+    list<User*> members;
+    int messages_counter;
 
 public:
     Chat();
     void addMessage(Message* msg);
+    list<Message*> getMessages();
     string showMessages();
+    int getMessagesCounter();
     int getId();
+    void addMember(User* usr);
+    list<User*> getMembers();
+    int getUnseenCounter();
+    int getSeenCounter();
 
 };
 
